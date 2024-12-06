c5b7d413645e76b251aac5871a437666c94a3206
Bryce? <bryce.jones2026@gbwl.org>
2024-12-06 14:40:43 +0000

    STUDENT EXIST NOW

---
+++ Student.java
@@ -0,0 +1,19 @@
+/*Example CLASS that defines an object
+Class names like Student are always capitalized
+Object-Orianited Classes are like BLUEPRINTS
+(in another Java Class, you create instances from this blueprint)
+*/
+public class Student {
+    // 1. INSTANCE VARIABLE (attributes/data)
+    //think of what this object needs to know
+String name; //"A student has a name"
+int gradYear;
+    //2.CONSTRUCTION (manuals for setting up an object)
+
+
+
+    // 3. METHODS (behaviors associated with that object)
+    
+    
+    
+}
